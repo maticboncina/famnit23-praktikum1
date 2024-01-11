2f3bd56408bfe1531aaa5f3346798562a06b7380
Matic Bončina <github@express.ninja>
2024-01-11 12:54:37 +0100

    popravke task_4

--- class/c/2024-01-10/rp1_c_2324-main/vaje_C_structures/pre/4_task/main.c
+++ class/c/2024-01-10/rp1_c_2324-main/vaje_C_structures/pre/4_task/main.c
@@ -1,3 +1,4 @@
+
 /* 
 Modify the program from previous task. Add support for adding students via 
 command line arguments.
@@ -26,70 +27,127 @@ Grade: a
 
 OPTIONAL TASK 1: Change code so that you are not limited to always provide detials for 3 students1.
 OPTIONAL TASK 2: Enable skipping inputs.
+
+Modify the program from previous task to support adding multiple sutdents.
+For example enable adding 3 students.
+
+Modify the code below to achive this goal.
+
+IZPIS: ./a.out
+
+Enter details for 3 students:
+Enter details for student 1:
+Enter student name: Nastja
+Enter roll number: 111
+Enter age: 21
+Enter grade: f
+Enter details for student 2:
+Enter student name: Simon
+Enter roll number: 222
+Enter age: 22
+Enter grade: d
+Enter details for student 3:
+Enter student name: Andreja
+Enter roll number: 333
+Enter age: 31
+Enter grade: a
+
+Displaying details for all students:
+
+Student 1 details:
+Student Name: Nastja
+Roll Number: 111
+Age: 21
+Grade: f
+
+Student 2 details:
+Student Name: Simon
+Roll Number: 222
+Age: 22
+Grade: d
+
+Student 3 details:
+Student Name: Andreja
+Roll Number: 333
+Age: 31
+Grade: a
+
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAX_STUDENTS 3
 
 // Define the Student structure
-struct Students {
+struct Student {
     char name[50];
     int roll_number;
     int age;
     char grade;
 };
 
-void inputStudentsCmd(struct Students *s, int *size, char *data[]) {
-    for (int i = 0; i < *size; i++) {
-        strcpy(s -> name, data[i * 4 + 1]);
-        s -> roll_number = atoi(data[i * 4 + 2]);
-        s -> age = atoi(data[i * 4 + 3]);
-        s -> roll_number = data[i * 4 + 4][0];
-        s += 1;
-    }
-    s -= *size;
+// Function to input data for a student
+void inputStudent(struct Student *s) {
+    printf("Enter student name: ");
+    scanf("%s", s->name);
+    printf("Enter roll number: ");
+    scanf("%d", &s->roll_number);
+    printf("Enter age: ");
+    scanf("%d", &s->age);
+    printf("Enter grade: ");
+    scanf(" %c", &s->grade);
 }
 
-void inputStudents(struct Students *s, int *size) {
-    for (int i = 0; i < *size; i++) {
-        printf("Enter details for student %i\nEnter student name: ", (i+1));
-        scanf("%s", s->name);
-        printf("Enter roll number: ");
-        scanf("%d", &s->roll_number);
-        printf("Enter age: ");
-        scanf("%d", &s->age);
-        printf("Enter grade: ");
-        scanf(" %c", &s->grade);
-        s++;
-    }
-    s -= *size;
+void inputStudentCMD(struct Student *s, char* argv[]) {
+	strcpy(s->name, argv[0]);
+	s->roll_number = atoi(argv[1]);
+	s->age = atoi(argv[2]);
+	s->grade = argv[3][0];
 }
 
 // Function to display student information
-void displayStudents(struct Students *s, int *size) {
-    for (int i = 0; i < *size; i++) {
-        printf("\nStudent %i details:\nStudent Name: %s\nRoll Number: %d\nAge: %d\nGrade: %c\n", (i+1), s -> name, s -> roll_number, s -> age, s -> grade);
-        s++;
-    }
-    s -= *size;
+void displayStudent(struct Student s) {
+    printf("\nStudent Name: %s\n", s.name);
+    printf("Roll Number: %d\n", s.roll_number);
+    printf("Age: %d\n", s.age);
+    printf("Grade: %c\n", s.grade);
 }
 
-int main(int argc, char* argv[]) {
-    int size = (argc - 1) / 4;
-    struct Students students[size];
-    if (size < MAX_STUDENTS) size = MAX_STUDENTS;
-    if(argc > 1) {
-        inputStudentsCmd(students, &size, argv);
-    }
-    else {
-        size = MAX_STUDENTS;
-        inputStudents(students, &size);
-    }
+int main(int args, char* argv[]) {
+	int size = (args-1)/4;
+	if (args != 1)
+	{
+		size = 3;
+	}
+	struct Student students[size];
+	
+	if (args != 1)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			inputStudentCMD(&students[i], argv+1+4*i);
+		}
 
-    displayStudents(students, &size);
-
-    printf("\n");
+		for (int i = 0; i < size; i++)
+    	{
+        	printf("\nStudent %i details:\n", 1+i);
+        	displayStudent(students[i]);
+    	}
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+    	{
+        	printf("Enter details for student %i:\n", i+1);
+        	inputStudent(&students[i]);
+    	}
+    
+    	for (int i = 0; i < size; i++)
+    	{
+        	printf("\nStudent %i details:\n", 1+i);
+        	displayStudent(students[i]);
+    	}
+	}
+	
     return 0;
 }
